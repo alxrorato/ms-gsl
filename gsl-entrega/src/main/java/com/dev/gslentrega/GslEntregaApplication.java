eeeeeb2d9e5c0ef01b86d0795d476a50227ddb18
alxrorato <alx.rorato.carneiro@gmail.com>
2021-09-23 12:05:40 -0300

    Projeto gsl-entrega criado

---
+++ gsl-entrega/src/main/java/com/dev/gslentrega/GslEntregaApplication.java
@@ -0,0 +1,13 @@
+package com.dev.gslentrega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GslEntregaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GslEntregaApplication.class, args);
+	}
+
+}
---
+++ gsl-entrega/src/test/java/com/dev/gslentrega/GslEntregaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.gslentrega;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GslEntregaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
