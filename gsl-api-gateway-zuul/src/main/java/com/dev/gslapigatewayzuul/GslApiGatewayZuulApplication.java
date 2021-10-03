7bf2fd0ba554824ea95d19a9ab078fb5e661b0ee
alxrorato <alx.rorato.carneiro@gmail.com>
2021-10-03 18:07:25 -0300

    Projeto gsl-api-gateway-zuul criado

---
+++ gsl-api-gateway-zuul/src/main/java/com/dev/gslapigatewayzuul/GslApiGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.dev.gslapigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GslApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GslApiGatewayZuulApplication.class, args);
+	}
+
+}
---
+++ gsl-api-gateway-zuul/src/test/java/com/dev/gslapigatewayzuul/GslApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.gslapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GslApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
