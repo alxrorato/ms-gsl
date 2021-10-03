e38037fae5150c4312ffd687c2f98981fd774105
alxrorato <alx.rorato.carneiro@gmail.com>
2021-10-03 13:48:18 -0300

    Configurando Eureka Clients

--- gsl-entrega/src/main/java/com/dev/gslentrega/GslEntregaApplication.java
+++ gsl-entrega/src/main/java/com/dev/gslentrega/GslEntregaApplication.java
@@ -2,10 +2,11 @@ package com.dev.gslentrega;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@RibbonClient(name = "gsl-cliente")
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
 public class GslEntregaApplication {
