d7b416bc7f3c02be18957230a680afa1c21213ed
alxrorato <alx.rorato.carneiro@gmail.com>
2021-10-09 18:33:11 -0300

    Criação e config. do projeto gsl-oauth

---
+++ gsl-oauth/src/test/java/com/dev/gsloauth/GslOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.gsloauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GslOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
